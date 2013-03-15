d38d37be12a4d81eb96ffbe2a84bd945cf2ad17a
chocoffice <2049423+chocolat0w0@users.noreply.github.com>
2013-03-15 11:55:29 +0900

    ソースコード1st commit

---
+++ src/hanabi/HANABi.java
@@ -0,0 +1,18 @@
+package hanabi;
+
+import processing.core.PApplet;
+
+
+public class HANABi extends PApplet {
+
+	public void setup() {
+		size(100, 100);
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { hanabi.HANABi.class.getName() });
+	}
+}
