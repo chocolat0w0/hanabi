3d6661f830be371ba8d4b0808ea1eabad3b932ce
chocoffice <shiori.yamazaki.1002@gmail.com>
2013-03-16 21:47:58 +0900

    花火複数個対応

--- src/hanabi/HANABiMain.java
+++ src/hanabi/HANABiMain.java
@@ -1,34 +1,50 @@
 package hanabi;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 
 public class HANABiMain extends PApplet {
 	
 	ViewController viewController = null;
-	HANABi[] hanabi = new HANABi[1];
+	ArrayList<HANABi> hanabi = new ArrayList<HANABi>();
 
 	public void setup() {
 		viewController = new ViewController(this);
 		size(500, 500);
 		frameRate(30);
 		background(80);
-		hanabi[0] = new HANABi(100, height);
+		
+		hanabi.add(new HANABi(100, height));
+		hanabi.add(new HANABi(200, height));
+		hanabi.add(new HANABi(300, height));
 	}
 
 	public void draw() {
 		background(80);
 		viewController.drawHanabi(hanabi);
-		hanabi[0].move();
+		for (HANABi h : hanabi) {
+			h.move();
+		}
 	}
 	
 	public void mousePressed() {
-		if((hanabi[0].x - 10) < mouseX && mouseX < (hanabi[0].x + 10) 
-				&& (hanabi[0].y - 10) < mouseY && mouseY < (hanabi[0].y + 10)) {
-			hanabi[0].catched = true;
-			viewController.catchHanabi(hanabi[0]);
+		for (HANABi h : hanabi) {
+			if((h.x - 10) < mouseX && mouseX < (h.x + 10) 
+					&& (h.y - 10) < mouseY && mouseY < (h.y + 10)) {
+				h.catched = true;
+				viewController.catchHanabi(h);
+			}
 		}
 	}
+	
+	public void keyPressed() {
+		if(key == 'a') {
+			background(255, 0, 0);
+		}
+	}
+	
 	public static void main(String _args[]) {
 		PApplet.main(new String[] { hanabi.HANABiMain.class.getName() });
 	}
