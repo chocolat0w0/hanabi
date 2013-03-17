2dc0d51c6d6a6412ce62612bbf5a34632a8228ce
chocoffice <2049423+chocolat0w0@users.noreply.github.com>
2013-03-17 11:28:39 +0900

    内部クラスを外に出したよ

--- src/hanabi/ViewController.java
+++ src/hanabi/ViewController.java
@@ -9,16 +9,16 @@ public class ViewController {
 	
 	private static final int EXPLODABLE_STEP = 30;
 
-	PApplet parent;
+	private PApplet parent;
 	private ArrayList<ExplodeHanabi> explodeHanabis = new ArrayList<ExplodeHanabi>();
-	Iterator<ExplodeHanabi> iter = explodeHanabis.iterator();
+	private Iterator<ExplodeHanabi> iter = explodeHanabis.iterator();
 	
 	public ViewController(PApplet parent) {
 		this.parent = parent;
 	}
 	
 	public void explode(HANABi hanabi) {
-		explodeHanabis.add(new ExplodeHanabi(hanabi));
+		explodeHanabis.add(new ExplodeHanabi(hanabi, parent));
 	}
 	
 	public void drawHanabi(ArrayList<HANABi> hanabi) {
@@ -58,71 +58,5 @@ public class ViewController {
 		parent.fill(255, 0, 0);
 		parent.ellipse(hanabi.x, hanabi.y, 30, 30);
 		parent.rect(hanabi.x, hanabi.y, 30, 30);
-	}
-	
-	class ExplodeHanabi {
-		float centerX;
-		float centerY;
-		int colorR;
-		int colorG;
-		int colorB;
-		float dx;
-		float dy;
-		float distance;
-		int step;
-		ArrayList<Spark> sparks = new ArrayList<Spark>();
-		
-		ExplodeHanabi(float x, float y) {
-			this.centerX = x;
-			this.centerY = y;
-			this.step = 0;
-			this.distance = 0;
-		}
-		
-		public ExplodeHanabi(HANABi hanabi) {
-			this.centerX = hanabi.x;
-			this.centerY = hanabi.y;
-			this.colorR = hanabi.r;
-			this.colorG = hanabi.g;
-			this.colorB = hanabi.b;
-		}
-
-		public boolean update() {
-			distance += 10;
-			step++;
-			if (EXPLODABLE_STEP < step) {
-				return false;
-			}
-			for (int i = 0; i < step * 4; i++) {
-				dx = (float) (distance * Math.sin(360 / (step * 4 * 2) * i));
-				dy = (float) (distance * Math.cos(360 / (step * 4 * 2) * i));
-				sparks.add(new Spark(centerX + dx, centerY + dy, step));
-			}
-			for (int i = sparks.size() - 1; i >= 0; i--) {
-				if (sparks.get(i).step == step - 10) {
-					sparks.remove(i);
-				}
-			}
-			return true;
-		}
-		
-		public void drawSparks() {
-			for(Spark spark : sparks) {
-				parent.fill(colorR, colorG, colorB);
-				parent.ellipse(spark.x, spark.y, 10, 10);
-			}
-		}
-	}
-	
-	class Spark {
-		float x;
-		float y;
-		int step;
-		
-		Spark(float x, float y, int step) {
-			this.x = x;
-			this.y = y;
-			this.step = step;
-		}
 	}
 }
